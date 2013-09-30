ceadc61958d7735128d537e4d9ff3114eb7dff70
inch-huang <p48991191@mail.ncku.edu.tw>
2013-09-30 17:50:36 +0800

    Read/Write file from a specific path.

--- hello.c
+++ hello.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILE_NAME "/etc/hosts"
-#define DATA_BUF_SIZ 512
+#define RW_FILE_NAME "/home/inch-huang/hw2_b.txt"
+#define DATA_BUF_SIZ 200
 
 int main(void)
 {
 	FILE *fd;
 	unsigned char Data_Buf[DATA_BUF_SIZ];
-
-	fd = fopen(FILE_NAME, "r");
+	char str_write[30] = "Here is HW2_b for Lab20.\r\n";
+	//Write into specific file
+	fd = fopen(RW_FILE_NAME, "w");
 	if(fd==NULL){
 		printf("Wrong file name or path\r\n");	
 		return 0;	
 	}
-	fread(&Data_Buf,sizeof(Data_Buf),1,fd);
-	printf("\r\nread from %s:\r\n%s",FILE_NAME, Data_Buf);	
+	printf("The string is going to write into %s is :\r\n%s",RW_FILE_NAME,str_write);
+	fprintf(fd,"%s",&str_write);
+	fclose(fd);
+	//Read out the file to verify
+	fd = fopen(RW_FILE_NAME, "r");
+	if(fd==NULL){
+		printf("Wrong file name or path\r\n");	
+		return 0;	
+	}	
+	fread(Data_Buf,sizeof(Data_Buf),1,fd);
+	printf("The string read from %s is :\r\n%s",RW_FILE_NAME,Data_Buf);	
 	fclose(fd);
 	return 0 ;
 }
