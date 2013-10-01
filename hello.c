dfa74cb23ebbb633132adf02aeb1876838074a84
inch-huang <p48991191@mail.ncku.edu.tw>
2013-10-01 15:47:56 +0800

    R/W a file under the host currnt path.

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define RW_FILE_NAME "/home/inch-huang/hw2_b.txt"
 #define DATA_BUF_SIZ 200
 
 int main(void)
@@ -9,23 +9,26 @@ int main(void)
 	FILE *fd;
 	unsigned char Data_Buf[DATA_BUF_SIZ];
 	char str_write[30] = "Here is HW2_b for Lab20.\r\n";
+
+	//Get current folder path 
+	system("pwd -P>dir");
 	//Write into specific file
-	fd = fopen(RW_FILE_NAME, "w");
+	fd = fopen("dir", "w");
 	if(fd==NULL){
 		printf("Wrong file name or path\r\n");	
 		return 0;	
 	}
-	printf("The string is going to write into %s is :\r\n%s",RW_FILE_NAME,str_write);
+	printf("The string is going to write into file is :\r\n%s",str_write);
 	fprintf(fd,"%s",&str_write);
 	fclose(fd);
 	//Read out the file to verify
-	fd = fopen(RW_FILE_NAME, "r");
+	fd = fopen("dir", "r");
 	if(fd==NULL){
 		printf("Wrong file name or path\r\n");	
 		return 0;	
 	}	
 	fread(Data_Buf,sizeof(Data_Buf),1,fd);
-	printf("The string read from %s is :\r\n%s",RW_FILE_NAME,Data_Buf);	
+	printf("The string read from file is :\r\n%s",Data_Buf);	
 	fclose(fd);
 	return 0 ;
 }
